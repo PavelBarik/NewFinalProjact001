28f345e5ac336e41d206426ed8c9277f2c329ace
Pavel <barantsev020@mail.ru>
2018-05-02 21:00:27 +0700

    1

--- app/src/main/java/com/example/pavel/finalpj10/NewTask.java
+++ app/src/main/java/com/example/pavel/finalpj10/NewTask.java
@@ -3,6 +3,7 @@ package com.example.pavel.finalpj10;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ public class NewTask extends AppCompatActivity {
 EditText edit1,edit2,edit3,edit4,edit5,edit6;
     String Data;
     TextView DaTa;
+    Button sAvE,DEL,BaCk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,15 @@ EditText edit1,edit2,edit3,edit4,edit5,edit6;
         DaTa = findViewById(R.id.Date);
         Data = TaskActivity.DATA2;
         DaTa.setText(Data);
+        edit1 = findViewById(R.id.Edit1);
+        edit2 = findViewById(R.id.Edit2);
+        edit3 = findViewById(R.id.Edit3);
+        edit4 = findViewById(R.id.Edit4);
+        edit5 = findViewById(R.id.Edit5);
+        edit6 = findViewById(R.id.Edit6);
+        sAvE = findViewById(R.id.SaVe);
+        DEL = findViewById(R.id.Del);
+        BaCk= findViewById(R.id.Back);
 
     }
 }
