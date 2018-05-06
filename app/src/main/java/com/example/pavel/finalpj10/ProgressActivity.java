3a0e38e4027c04f034e718f9d01816b356481bab
Pavel <barantsev020@mail.ru>
2018-05-06 22:23:38 +0700

    1

--- app/src/main/java/com/example/pavel/finalpj10/ProgressActivity.java
+++ app/src/main/java/com/example/pavel/finalpj10/ProgressActivity.java
@@ -1,13 +1,31 @@
 package com.example.pavel.finalpj10;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ProgressActivity extends AppCompatActivity {
-
+Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                i = new Intent(ProgressActivity.this, MainActivity.class );
+                startActivity(i);
+                ProgressActivity.this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
