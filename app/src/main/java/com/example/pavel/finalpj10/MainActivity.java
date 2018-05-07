298a6445e4fc0c6a201803452eec0e93d5dfd0be
Pavel <barantsev020@mail.ru>
2018-05-07 16:05:17 +0700

    1

--- app/src/main/java/com/example/pavel/finalpj10/MainActivity.java
+++ app/src/main/java/com/example/pavel/finalpj10/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pavel.finalpj10;
 
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     Intent i;
     DBHelper dbHelper;
     CalendarView calendarView;
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
