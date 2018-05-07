68f43df83071a2273749a881bd0c30858503f032
Pavel <barantsev020@mail.ru>
2018-05-07 16:55:11 +0700

    1

--- app/src/main/java/com/example/pavel/finalpj10/MainActivity.java
+++ app/src/main/java/com/example/pavel/finalpj10/MainActivity.java
@@ -3,24 +3,60 @@ package com.example.pavel.finalpj10;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Intent i;
     DBHelper dbHelper;
     CalendarView calendarView;
+    ImageView imageView;
+    TextView num,zv,text;
+
+    int check,check1,check2;
     Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView1);
+        dialog = new Dialog(MainActivity.this);
+        imageView = findViewById(R.id.image_progress);
+        text = dialog.findViewById(R.id.text_progress);
+        // Установите заголовок
+        dialog.setTitle("Достижение");
+        // Передайте ссылку на разметку
+        dialog.setContentView(R.layout.dialog);
+        // Найдите элемент TextView внутри вашей разметки
+        // и установите ему соответствующий текст
 
+
+        check = 1;
+        dbHelper = new DBHelper(this);
+        dbHelper = new DBHelper(getBaseContext());
+        SQLiteDatabase db;
+        db = dbHelper.getReadableDatabase();
+        String SRAVNENIE = "SELECT * FROM " + DBHelper.TABLE_CONTACTS + " WHERE " + DBHelper.KEY_CHECK + "=" + "\"" + check + "\"" + ";";
+        Cursor cursor1 = db.rawQuery(SRAVNENIE, null);
+        cursor1.moveToFirst();
+        check1 = cursor1.getCount();
+        check2 = cursor1.getCount();
+
+
+        if (check2 == 3) {
+            imageView.setImageResource(R.drawable.progress);
+            text.setText("Спасибо за то что вы используете наше приложение. Вот вам награда!");
+            dialog.show();
+        }
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
