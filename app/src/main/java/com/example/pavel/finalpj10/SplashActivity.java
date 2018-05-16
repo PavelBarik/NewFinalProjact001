6a613cb3690ec0c2cfb61195f96c53dd290025fb
Pavel <barantsev020@mail.ru>
2018-05-16 14:51:58 +0700

    1

--- app/src/main/java/com/example/pavel/finalpj10/SplashActivity.java
+++ app/src/main/java/com/example/pavel/finalpj10/SplashActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
     Intent intent;
+    TextView textView;
+    long timer_s = 0;
+    long timer_d = 500;
+    long timer_f = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,9 @@ public class SplashActivity extends AppCompatActivity {
         ImageView logo = (ImageView) findViewById(R.id.Logo);
         Animation starAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         logo.startAnimation(starAnim);
+        textView=findViewById(R.id.textView);
 
+        
         intent = new Intent(SplashActivity.this, MainActivity.class);
 
         starAnim.setAnimationListener(new Animation.AnimationListener() {
